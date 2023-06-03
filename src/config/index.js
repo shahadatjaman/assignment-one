ad7d0248a5288719be74d3d7ff6491a7c303289c
Shahadat Jaman <devloperabuhuraira@gmail.com>
2023-06-03 18:48:47 +0600

    modified firebase confg

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBftihHu3Y5XVAEHFNzredrhdNsGmMXxBs",
-  authDomain: "stockgro-1ebcd.firebaseapp.com",
-  projectId: "stockgro-1ebcd",
-  storageBucket: "stockgro-1ebcd.appspot.com",
-  messagingSenderId: "502176370030",
-  appId: "1:502176370030:web:818764c79f4ae765df457f",
-  measurementId: "G-N3457KSFKM",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
